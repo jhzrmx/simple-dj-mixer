4b10fe089e4f6695c22d814e36664a34664abfd8
jhzrmx <122547859+jhzrmx@users.noreply.github.com>
2026-04-08 21:48:26 +0800

    chore: set base URL and build target in Vite config to configure in github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 import tailwindcss from "@tailwindcss/vite";
 
+const repositoryTarget = 'simple-dj-mixer/'
+
 export default defineConfig({
   plugins: [solid(), tailwindcss()],
+  base: `/${repositoryTarget}`,
+  build: {
+    target: 'esnext',
+  },
 })
